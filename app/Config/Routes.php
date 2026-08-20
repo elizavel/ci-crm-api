0d5da2ff3391f4b0f299876a447959f81da63cff
Eliza Rosario <eliza.rosario@tracktile.io>
2026-08-20 22:59:45 +0800

    use home as landing core

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /** @var RouteCollection $routes */
 
 // landing page
-$routes->get('/', 'UserAccount::login');
-$routes->get('/signup', 'UserAccount::signup'); 
-$routes->get('/logout', 'UserAccount::logout');
+$routes->get('/', 'Home::login');
+$routes->get('/signup', 'Home::signup'); 
+$routes->get('/logout', 'Home::logout');
 $routes->get('/dashboard', 'Home::dashboard');
 
+// UserAccount
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,4 +13,24 @@ class Home extends BaseController
     {
         return view('landing/dashboard');
     }
+
+     public function signup(): string
+    {
+        return view('landing/signup');
+    }
+
+    public function verifyAccount(): string
+    {
+        return view('landing/verify_email');
+    }
+
+    public function login(): string
+    {
+        return view('landing/login');
+    }
+
+    public function logout(): string
+    {
+        return view('landing/logout');
+    }
 }
